616742a37bc531c0301b2268390988e009e535ee
adam <everett.ingram22@gmail.com>
2022-03-30 13:29:08 -0400

    Updated Comment.

--- sandbox/src/main/java/week5/Day3.java
+++ sandbox/src/main/java/week5/Day3.java
@@ -1,6 +1,7 @@
 package week5;
 
 public class Day3 {
-    //9 to 1 work on making mini project a working application
-    //At 1 PM after lunch we will start working with files
+    //9 to 3 work on making mini project a working application
+    //At 3 PM after lunch we will look at interfaces again and
+    //then start working with files
 }
