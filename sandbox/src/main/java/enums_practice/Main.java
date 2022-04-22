4076bcc6c83f47062d2f1e46e4c38eaab959f8e7
adam <everett.ingram22@gmail.com>
2022-04-22 09:28:24 -0400

    Added Enums

--- sandbox/src/main/java/enums_practice/Main.java
+++ sandbox/src/main/java/enums_practice/Main.java
@@ -1,9 +1,54 @@
 package enums_practice;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("ENUM practice!");
+import java.util.Objects;
 
-        
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Color enum
+        Color color1 = Color.BLUE;
+        System.out.println("\n" + color1 + "\n");
+
+        // Calling values()
+        Color[] colorValue = Color.values();
+
+        // enum with loop
+        for (Color col : colorValue) {
+            // Calling ordinal() to find index
+            System.out.println(col + " at index " + col.ordinal());
+        }
+
+        //Getting the weather
+        Day weather = new Day(Days.MONDAY);
+        System.out.println();
+        System.out.println(weather.currentDaysWeather(weather.getCurrentDay()));
+
+
+
+
+        //**************************************************************************
+        //Vehicle Enums
+        Ford ford = Ford.MUSTANG;
+        GMC gmc = GMC.YUKON;
+
+    }
+
+
+
+
+    //Declaring vehicle enums inside a class
+    enum Ford {
+        MUSTANG,
+        F150,
+        F250,
+        ESCAPE,
+        BRONCO
+    }
+    enum GMC {
+        SIERRA,
+        YUKON,
+        ACADIA,
+        TERRAIN
     }
 }
